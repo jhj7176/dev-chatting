ce808d378320271f4c190e798c733e44df6673df
장서현 <68838251+rxjang@users.noreply.github.com>
2022-06-26 15:28:57 +0900

    feature (#3): Add db setting config (#5)
    * feature (#3): Add db setting config
    
    * hotfix (#3): Add missing file

---
+++ src/test/java/com/chat/dev/config/JasyptConfigTest.java
@@ -0,0 +1,29 @@
+package com.chat.dev.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptConfigTest {
+
+    @Test
+    void jasypt() {
+        String url= "";
+        String username = "";
+        String password = "";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "salpa_chatting";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+}
